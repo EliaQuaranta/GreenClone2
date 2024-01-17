b19d14b62fe189f8c1f598b92feea8e669aa9a11
Federico Bempensato <bempensato.federico@gmail.com>
2024-01-17 18:40:43 +0100

    fixes blocco analisi

--- src/app/components/Analisi.tsx
+++ src/app/components/Analisi.tsx
@@ -1,107 +1,120 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 function Analisi() {
+  return (
+    <div>
+      <div className="flex flex-wrap lg:flex-nowrap justify-center max-w-5xl p-10 xl:p-0 mx-auto gap-10">
+        <div className="lg:w-1/2">
+          <h1 className="  font-bold text-5xl pt-10 leading-snug">
+            Di cosa hai bisogno per il tuo{" "}
+            <span className=" text-cyan-400">Business</span> ?
+          </h1>
 
-    return(
-<div>
-        <div className='columns-2 columns-md-1 flex flex-wrap justify-center'>
-        
-        <div>
-            <h1 className=' pl-10 font-bold text-6xl pt-20'>Di cosa hai bisogno per il tuo <span className=' text-cyan-400'>Business</span> ?</h1>
-           
-            <h4 className=' pl-10 fontHead text-xl text-clip pt-8'>La strategia pubblicitaria giusta per dare valore al budget investito </h4>
-            
-            <h4 className='pl-10 font-bold text-xl pt-10'>Hai scelto i canali giusti per la tua pubblicità? Sei consapevole di come viene speso il tuo budget pubblicitario? Stai ottenendo i risultati attesi?</h4>
-            
-            <h4 className='pl-10 font-bold text-xl pt-2'>Green.Click costruisce la strategia più adeguata per la tua azienda. Seguiamo il tuo business lungo tutto il percorso di crescita online!</h4>
-        
-            <button className='bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md ml-10 mt-7'>TUTTI I SERVIZI</button>
-        
-        </div>
-        
-        
-        <div className=' flex-wrap'>
-        
-        <img src="\hand-made-green-click.jpg" className='pt-8 min-h-2 max-w-md ' alt="" />
-        
-        </div>
-           
-           </div>
-           
-           
-        
+          <h4 className="  fontHead text-lg text-clip pt-5">
+            La strategia pubblicitaria giusta per dare valore al budget
+            investito{" "}
+          </h4>
 
+          <h4 className="  text-lg pt-10">
+            Hai scelto i canali giusti per la tua pubblicità? Sei consapevole di
+            come viene speso il tuo budget pubblicitario? Stai ottenendo i
+            risultati attesi?
+          </h4>
 
-           <div className='columns-2 columns-md-1 flex flex-wrap justify-end'>
-           <div className=' '>
-            
-            <img src="\hand-made-green-click.jpg" className='pt-8 min-h-2 max-w-md ' alt="" />
-            </div>
-        <div>
-            <h1 className=' pl-10 font-bold text-6xl pt-20'>Di cosa hai bisogno per il tuo <span className=' text-cyan-400'>Business</span> ?</h1>
-           
-            <h4 className=' pl-10 fontHead text-xl text-clip pt-8'>La strategia pubblicitaria giusta per dare valore al budget investito </h4>
-            
-            <h4 className='pl-10 font-bold text-xl pt-10'>Hai scelto i canali giusti per la tua pubblicità? Sei consapevole di come viene speso il tuo budget pubblicitario? Stai ottenendo i risultati attesi?</h4>
-            
-            <h4 className='pl-10 font-bold text-xl pt-2'>Green.Click costruisce la strategia più adeguata per la tua azienda. Seguiamo il tuo business lungo tutto il percorso di crescita online!</h4>
-        
-            <button className='bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md ml-10 mt-7'>Analisi Gratuita</button>
-        
-        
-        
-        
+          <h4 className="   text-lg pt-2">
+            Green.Click costruisce la strategia più adeguata per la tua azienda.
+            Seguiamo il tuo business lungo tutto il percorso di crescita online!
+          </h4>
+
+          <button className="bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md  mt-7">
+            TUTTI I SERVIZI
+          </button>
         </div>
-        
-           
-           </div>
-           <div className='main'>
-    <div className=' bg-[#F5F1EE] '>
-    <h1 className='text-center pt-20 font-extrabold text-3xl '>Dicono di noi...  </h1>
-    
-   
-    </div>
-    <div className="flex w-full h-64 bg-[#F5F1EE] pt-10">
-  <div className="grid w-1/3 h-20 flex-grow card rounded-box place-items-center">
-    
-    <h1 className='text-2xl font-bold fontHead'>Sulla Crescita del Progetto Affidato</h1>
-    {/* <h3 className=' text-center font-bold'>Troviamo la strategia online più efficace per ogni business e la rendiamo scalabile.</h3>
+
+        <div className="lg:w-1/2 flex items-center">
+          <img
+            src="\hand-made-green-click.jpg"
+            className="pt-8 min-h-2 w-full"
+            alt=""
+          />
+        </div>
+      </div>
+
+      <div className="flex flex-wrap-reverse lg:flex-nowrap justify-center max-w-5xl p-10 xl:p-0 mx-auto gap-10">
+        <div className="lg:w-1/2 flex items-center">
+          <img
+            src="https://www.greenclickmedia.it/wp-content/uploads/2023/09/rosa-green-click.jpg"
+            className="pt-8 min-h-2 w-full"
+            alt=""
+          />
+        </div>
+        <div className="lg:w-1/2">
+          <h1 className="  font-bold text-5xl pt-20 leading-snug">
+            Di cosa hai bisogno per il tuo{" "}
+            <span className=" text-cyan-400">Business</span> ?
+          </h1>
+
+          <h4 className="  fontHead text-lg text-clip pt-5">
+            La strategia pubblicitaria giusta per dare valore al budget
+            investito{" "}
+          </h4>
+
+          <h4 className="  text-lg pt-10">
+            Hai scelto i canali giusti per la tua pubblicità? Sei consapevole di
+            come viene speso il tuo budget pubblicitario? Stai ottenendo i
+            risultati attesi?
+          </h4>
+
+          <h4 className="   text-lg pt-2">
+            Green.Click costruisce la strategia più adeguata per la tua azienda.
+            Seguiamo il tuo business lungo tutto il percorso di crescita online!
+          </h4>
+
+          <button className="bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md  mt-7">
+            TUTTI I SERVIZI
+          </button>
+        </div>
+      </div>
+
+      <div className="main">
+        <div className=" bg-[#F5F1EE] ">
+          <h1 className="text-center pt-10 font-extrabold text-3xl ">
+            Dicono di noi...{" "}
+          </h1>
+        </div>
+        <div className="flex w-full h-64 bg-[#F5F1EE] pt-10">
+          <div className="grid w-1/3 h-20 flex-grow card rounded-box place-items-center">
+            <h1 className="text-2xl font-bold fontHead">
+              Sulla Crescita del Progetto Affidato
+            </h1>
+            {/* <h3 className=' text-center font-bold'>Troviamo la strategia online più efficace per ogni business e la rendiamo scalabile.</h3>
     <h3 className=' text-center'>Il tuo obiettivo è il nostro obiettivo.</h3> */}
+          </div>
 
+          <div className="divider divider-horizontal colorDiveder"></div>
 
-  </div>
-  
-  <div className="divider divider-horizontal colorDiveder"></div>
-  
-  <div className="grid w-1/3 h-20 flex-grow card  rounded-box place-items-center">
-    
-    <h1 className='text-2xl font-bold fontHead'>Sui Risultati</h1>
-    {/* <h3 className=' text-center font-bold'>Formazione, aggiornamento ed esperienza sono nel nostro DNA.</h3>
+          <div className="grid w-1/3 h-20 flex-grow card  rounded-box place-items-center">
+            <h1 className="text-2xl font-bold fontHead">Sui Risultati</h1>
+            {/* <h3 className=' text-center font-bold'>Formazione, aggiornamento ed esperienza sono nel nostro DNA.</h3>
     <h3 className=' text-center'>Non sono i settori di mercato o le piattaforme pubblicitarie a fare la differenza ma la nostra competenza.</h3> */}
+          </div>
+          <div className="divider divider-horizontal"></div>
+          <div className="grid w-1/3 h-20 flex-grow card  rounded-box place-items-center">
+            <h1 className="text-2xl font-bold fontHead">
+              Sulla Cura del cliente
+            </h1>
 
-    </div>
-  <div className="divider divider-horizontal"></div>
-  <div className="grid w-1/3 h-20 flex-grow card  rounded-box place-items-center">
-    
-    <h1 className='text-2xl font-bold fontHead'>Sulla Cura del cliente</h1>
-
-      {/* <h3 className=' text-center font-bold'>Crediamo nella trasparenza,
+            {/* <h3 className=' text-center font-bold'>Crediamo nella trasparenza,
         in un rapporto di confronto
         e collaborazione continua con il cliente.</h3> */}
 
-    {/* <h3>Ad ogni progetto diamo presenza, attenzione e cura.</h3>
+            {/* <h3>Ad ogni progetto diamo presenza, attenzione e cura.</h3>
     <h3>Da noi trovi sempre qualcuno che ti risponde quando chiami!</h3> */}
-    
-    
-  </div>
-</div>
-   
+          </div>
+        </div>
+      </div>
     </div>
-        
-</div>
-    )
-
+  );
 }
 
 export default Analisi;
--- src/app/components/carousel.tsx
+++ src/app/components/carousel.tsx
@@ -1,56 +1,98 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 function Carousel() {
+  return (
+    <div>
+      <div className="columns-2 columns-md-1">
+        <div>
+          <h1 className="font-bold text-6xl text-start pl-10  pt-20">
+            Casi di <span className="text-cyan-400">successo</span>
+          </h1>
 
-    return(
+          <h2 className="font-bold text-xl text-start pl-10 fontHead pt-7">
+            Ti raccontiamo come è andata con la gestione Green.Click
+          </h2>
 
-<div>
-        <div className='columns-2 columns-md-1'>
-            <div>
-                <h1 className='font-bold text-6xl text-start pl-10  pt-20'>Casi di <span className='text-cyan-400'>successo</span></h1>
-            
-            <h2 className='font-bold text-xl text-start pl-10 fontHead pt-7'>Ti raccontiamo come è andata con la gestione Green.Click</h2>
-                
-                <h4 className='font-bold text-xl text-start pl-10 pt-7'>Giornalmente veniamo a contatto e ascoltiamo tante storie, progetti, idee. Ci occupiamo dello sviluppo di strategie con tante caratteristiche diverse. 
-                <br />Siamo convinti che per ogni business online si possa trovare la strada giusta per crescere. <br /> In questo spazio ti proponiamo alcuni casi studio per mostrarti come lavoriamo.
-                
-                <button className='bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md mt-3'> <h1 className='flex pl-4 items-center'>Scopri di più<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
-</svg>
-</h1> </button>
-                
-                </h4>
+          <h4 className="font-bold text-xl text-start pl-10 pt-7">
+            Giornalmente veniamo a contatto e ascoltiamo tante storie, progetti,
+            idee. Ci occupiamo dello sviluppo di strategie con tante
+            caratteristiche diverse.
+            <br />
+            Siamo convinti che per ogni business online si possa trovare la
+            strada giusta per crescere. <br /> In questo spazio ti proponiamo
+            alcuni casi studio per mostrarti come lavoriamo.
+            <button className="bg-cyan-400 p-6 mb-16 border-cyan-900 text-white font-bold rounded-md mt-3">
+              {" "}
+              <h1 className="flex pl-4 items-center">
+                Scopri di più
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+                  />
+                </svg>
+              </h1>{" "}
+            </button>
+          </h4>
 
-                
-                
-                <div className="carousel w-full h-96 pt-20">
-  <div id="item1" className="carousel-item w-full">
-    <img src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
-  </div> 
-  <div id="item2" className="carousel-item w-full">
-    <img src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
-  </div> 
-  <div id="item3" className="carousel-item w-full">
-    <img src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
-  </div> 
-  <div id="item4" className="carousel-item w-full">
-    <img src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
-  </div>
-</div> 
-<div className="flex justify-center w-full py-2 gap-2">
-  <a href="#item1" className="btn btn-xs rounded-full bg-slate-600">1</a> 
-  <a href="#item2" className="btn btn-xs rounded-full bg-slate-600">2</a> 
-  <a href="#item3" className="btn btn-xs rounded-full bg-slate-600">3</a> 
-  <a href="#item4" className="btn btn-xs rounded-full bg-slate-600">4</a>
-</div>
+          <div className="carousel w-full h-96 pt-20">
+            <div id="item1" className="carousel-item w-full">
+              <img
+                src="https://daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg"
+                className="w-full"
+              />
             </div>
+            <div id="item2" className="carousel-item w-full">
+              <img
+                src="https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
+                className="w-full"
+              />
+            </div>
+            <div id="item3" className="carousel-item w-full">
+              <img
+                src="https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg"
+                className="w-full"
+              />
+            </div>
+            <div id="item4" className="carousel-item w-full">
+              <img
+                src="https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
+                className="w-full"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center w-full py-2 gap-2">
+            <a
+              href="#item1"
+              className="h-8 w-8 border border-black flex items-center justify-center rounded-full"
+            >
+              <div className="h-2 w-2 bg-black rounded-full"></div>
+            </a>
+            <a
+              href="#item2"
+              className="h-8 w-8 hover:border transition-all border-transparent hover:border-black duration-300 flex items-center justify-center rounded-full"
+            >
+              <div className="h-2 w-2 bg-black rounded-full"></div>
+            </a>
+            <a href="#item3" className="btn btn-xs rounded-full bg-slate-600">
+              3
+            </a>
+            <a href="#item4" className="btn btn-xs rounded-full bg-slate-600">
+              4
+            </a>
+          </div>
         </div>
-
-
-</div>
-    )
-
+      </div>
+    </div>
+  );
 }
 
 export default Carousel;
